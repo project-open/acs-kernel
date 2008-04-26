ba3959333ebfc95dbc31aea983373d661ea736cc
Frank Bergmann <frank.bergmann@project-open.com>
2008-04-26 16:50:10 +0000

    - added v_count to acs_permission__xxx

--- sql/postgresql/acs-permissions-create.sql
+++ sql/postgresql/acs-permissions-create.sql
@@ -264,20 +264,26 @@ comment on table acs_privilege_hierarchy is '
  privilege is a superset of the bar privilege.
 ';
 
-create function acs_privilege__create_privilege (varchar,varchar,varchar)
+create or replace function acs_privilege__create_privilege (varchar,varchar,varchar)
 returns integer as '
 declare
-  create_privilege__privilege              alias for $1;  
-  create_privilege__pretty_name            alias for $2;  -- default null  
-  create_privilege__pretty_plural          alias for $3;  -- default null
+	create_privilege__privilege             alias for $1;  
+	create_privilege__pretty_name           alias for $2;  -- default null  
+	create_privilege__pretty_plural         alias for $3;  -- default null
+	v_count					integer;
 begin
-    insert into acs_privileges
-     (privilege, pretty_name, pretty_plural)
-    values
-     (create_privilege__privilege, 
-      create_privilege__pretty_name, 
-      create_privilege__pretty_plural);
-      
+	select count(*) into v_count from acs_privileges
+	where privilege = create_privilege__privilege;
+	IF v_count > 0 THEN return 0; END IF;
+
+	insert into acs_privileges (
+		privilege, pretty_name, pretty_plural
+	) values (
+		create_privilege__privilege, 
+		create_privilege__pretty_name, 
+		create_privilege__pretty_plural
+	);
+
     return 0; 
 end;' language 'plpgsql';
 
@@ -301,16 +307,22 @@ begin
     return 0; 
 end;' language 'plpgsql';
 
-create function acs_privilege__add_child (varchar,varchar)
+create or replace function acs_privilege__add_child (varchar,varchar)
 returns integer as '
 declare
-  add_child__privilege              alias for $1;  
-  add_child__child_privilege        alias for $2;  
+	add_child__privilege            alias for $1;  
+	add_child__child_privilege      alias for $2;  
+	v_count				integer;
 begin
-    insert into acs_privilege_hierarchy
-     (privilege, child_privilege)
-    values
-     (add_child__privilege, add_child__child_privilege);
+	select count(*) into v_count from acs_privilege_hierarchy
+	where privilege = add_child__privilege and child_privilege = add_child__child_privilege;
+	IF v_count > 0 THEN return 0; END IF;
+	
+	insert into acs_privilege_hierarchy (
+		privilege, child_privilege
+	) values (
+		add_child__privilege, add_child__child_privilege
+	);
 
     return 0; 
 end;' language 'plpgsql';

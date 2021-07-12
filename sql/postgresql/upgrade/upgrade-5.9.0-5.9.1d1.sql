86580d2c1498e16ca357bfed991749a07aff9d5d
Frank Bergmann <frank.bergmann@project-open.com>
2021-07-12 10:48:03 -0700

    - Fixed issue with lexcelera upgrade

--- sql/postgresql/upgrade/upgrade-5.9.0-5.9.1d1.sql
+++ sql/postgresql/upgrade/upgrade-5.9.0-5.9.1d1.sql
@@ -4,6 +4,12 @@ delete from acs_permissions where grantee_id in(
         select object_id from acs_objects where object_type = 'user' and object_id not in (select party_id from parties)
 );
 
+-- Fraber 2021-07-11:
+-- Apparently there are zombie entries in acs_permissions
+delete from acs_permissions
+where grantee_id in (select distinct grantee_id from acs_permissions except all select object_id from acs_objects);
+
+
 ALTER TABLE acs_permissions DROP CONSTRAINT IF EXISTS acs_permissions_grantee_id_fk;
 ALTER TABLE acs_permissions ADD CONSTRAINT  acs_permissions_grantee_id_fk
       FOREIGN KEY (grantee_id) REFERENCES parties(party_id) ON DELETE CASCADE;
